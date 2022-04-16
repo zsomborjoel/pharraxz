81d688f716df4ec920770d3f7b0c257876de2e0e
zsomborjoel <zsomborjoel@gmail.com>
2022-04-16 22:44:14 +0200

    remove test code

--- backend/src/main/java/com/opensource/pharraxz/PharraxzApplication.java
+++ backend/src/main/java/com/opensource/pharraxz/PharraxzApplication.java
@@ -2,13 +2,6 @@ package com.opensource.pharraxz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import reactor.core.publisher.Mono;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.path;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class PharraxzApplication {
@@ -16,25 +9,5 @@ public class PharraxzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PharraxzApplication.class, args);
 	}
-
-	@Bean
-	HelloHandler helloHandler() {
-		return new HelloHandler();
-	}
-
-	@Bean
-	RouterFunction<?> routerFunction(HelloHandler helloHandler) {
-		return route(path("/hello"), r -> helloHandler.hello()
-				.flatMap(b -> ok().syncBody(b)));
-	}
-
-	public class HelloHandler {
-
-		public Mono<String> hello() {
-			return Mono.just("hello");
-		}
-	}
-
-
 
 }
