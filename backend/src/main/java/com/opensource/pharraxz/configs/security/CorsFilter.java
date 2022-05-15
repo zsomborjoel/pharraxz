cbc0546ae068f522176ea4bde5fe6c17f9be677f
zsomborjoel <zsomborjoel@gmail.com>
2022-05-15 12:19:23 +0200

    Specify cors methods and headers

--- backend/src/main/java/com/opensource/pharraxz/configs/security/CorsFilter.java
+++ backend/src/main/java/com/opensource/pharraxz/configs/security/CorsFilter.java
@@ -6,12 +6,15 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class CorsFilter implements WebFluxConfigurer {
+    private static final String[] CORS_HEADERS = {"Authorization", "Cache-Control", "Content-Type"};
+    private static final String[] CORS_METHODS = {"GET", "POST", "PUT", "DELETE", "PATCH"};
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedMethods(CORS_METHODS)
+                .allowedHeaders(CORS_HEADERS);
     }
 
 }
