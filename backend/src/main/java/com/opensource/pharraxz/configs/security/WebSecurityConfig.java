c55a910096bc7f69eb29b1a0dbde0ff913f630da
zsomborjoel <zsomborjoel@gmail.com>
2022-05-16 22:02:40 +0200

    Change cors config

--- backend/src/main/java/com/opensource/pharraxz/configs/security/WebSecurityConfig.java
+++ backend/src/main/java/com/opensource/pharraxz/configs/security/WebSecurityConfig.java
@@ -10,15 +10,26 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @AllArgsConstructor
 public class WebSecurityConfig {
 
+    private static final List<String> ALLOWED_HEADERS = List.of("Authorization", "Cache-Control", "Content-Type");
+    private static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE");
+    private static final List<String> ALLOWED_ORIGIN = Collections.singletonList("*");
+
     private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -26,8 +37,20 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(ALLOWED_HEADERS);
+        configuration.setAllowedMethods(ALLOWED_METHODS);
+        configuration.setAllowedOrigins(ALLOWED_ORIGIN);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint((swe, e) ->
                         Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))
