472ee351a798f867810b17319036cd194d2191b8
zsomborjoel <zsomborjoel@gmail.com>
2022-07-09 15:18:44 +0200

    Doctor Order table columns data show up properly

--- frontend/src/components/order/DoctorOrderTable.tsx
+++ frontend/src/components/order/DoctorOrderTable.tsx
@@ -5,44 +5,98 @@ import { DataGrid, GridColDef, GridSelectionModel, GridValueGetterParams } from 
 import { DoctorOrderOverview } from '../../services/model/DoctorOrderOverview';
 import { DoctorOrderDetail } from '../../services/model/DoctorOrderDetail';
 
-export type DcotorOrderTableProps = {
+export type DoctorOrderTableProps = {
     doctorOrderOverviews: DoctorOrderOverview[],
     selectedOrderDetailId: string | undefined,
     loading: boolean
 }
 
-const DoctorOrderTable: FC<DcotorOrderTableProps> = (
+const DoctorOrderTable: FC<DoctorOrderTableProps> = (
     {
         doctorOrderOverviews,
         selectedOrderDetailId,
         loading,
     },
 ) => {
-    const columns: GridColDef[] = [
-        { field: 'doctorOrderDetail.orderDetailId', headerName: 'Sub order id', width: 130 },
-        { field: 'doctorOrderDetail.product.name', headerName: 'Medicine name', width: 130 },
-        { field: 'doctorOrderDetail.product.registerNumber', headerName: 'Register number', width: 130 },
-        { field: 'doctorOrderDetail.product.packaging', headerName: 'Packaging', width: 130 },
-        { field: 'doctorOrderDetail.product.description', headerName: 'Medicie description', width: 130 },
-        { field: 'doctorOrderDetail.product.inn', headerName: 'Inn', width: 130 },
-        { field: 'doctorOrderDetail.product.releasable', headerName: 'Releasable', width: 130 },
-        { field: 'doctorOrderDetail.quantity', headerName: 'Quantity', width: 130 },
-        { field: 'doctorOrderDetail.oderType', headerName: 'Oder type', width: 130 },
-        { field: 'doctorOrderDetail.startDate', headerName: 'Order start date', width: 130 },
-        { field: 'doctorOrderDetail.endDate', headerName: 'Order end date', width: 130 },
-    ];
-
-    console.log(doctorOrderOverviews);
     const rows = doctorOrderOverviews;
+
+    const columns: GridColDef[] = [
+        {
+            field: 'doctorOrderDetail.orderDetailId',
+            headerName: 'Sub order id',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.orderDetailId,
+        },
+        {
+            field: 'doctorOrderDetail.product.name',
+            headerName: 'Medicine name',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.name,
+        },
+        {
+            field: 'doctorOrderDetail.product.registerNumber',
+            headerName: 'Register number',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.registerNumber,
+        },
+        {
+            field: 'doctorOrderDetail.product.packaging',
+            headerName: 'Packaging',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.packaging,
+        },
+        {
+            field: 'doctorOrderDetail.product.description',
+            headerName: 'Medicie description',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.description,
+        },
+        {
+            field: 'doctorOrderDetail.product.inn',
+            headerName: 'Inn',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.inn,
+        },
+        {
+            field: 'doctorOrderDetail.product.releasable',
+            headerName: 'Releasable',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.product.releasable,
+        },
+        {
+            field: 'doctorOrderDetail.quantity',
+            headerName: 'Quantity',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.quantity,
+        },
+        {
+            field: 'doctorOrderDetail.oderType',
+            headerName: 'Oder type',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.oderType,
+        },
+        {
+            field: 'doctorOrderDetail.startDate',
+            headerName: 'Order start date',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.startDate,
+        },
+        {
+            field: 'doctorOrderDetail.endDate',
+            headerName: 'Order end date',
+            width: 130,
+            valueGetter: (e) => e.row.doctorOrderDetail.endDate,
+        },
+    ];
 
     const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
 
     return (
         <div style={{ height: 800, width: '100%' }}>
-            <DataGrid rows={rows}
-                getRowId={(row) => row.doctorOrderDetail.orderDetailId}
+            <DataGrid getRowId={(row) => row.doctorOrderDetail.orderDetailId}
+                rows={rows}
                 columns={columns}
-                pageSize={30}
+                pageSize={5}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
                 autoHeight
--- frontend/src/services/model/DoctorOrderOverview.ts
+++ frontend/src/services/model/DoctorOrderOverview.ts
@@ -1,6 +1,7 @@
 import { DoctorOrderDetail } from './DoctorOrderDetail';
 
 export interface DoctorOrderOverview {
+    id: string;
     orderId: number;
     description: string;
     doctorOrderDetail: DoctorOrderDetail;
